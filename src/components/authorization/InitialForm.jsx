a297d39f30ae832bcea27a33a3e888099f7d75c5
LenDoc <lena.kirilenko13@gmail.com>
2022-08-14 23:32:29 +0300

    Started doing adaptive design (Login Page)

--- src/components/authorization/InitialForm.jsx
+++ src/components/authorization/InitialForm.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import { Button, Input, Checkbox, Form } from 'antd'
-import { Typography } from 'antd'
-
-const { Text } = Typography
+import { Typography, Col, Row } from 'antd'
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+const { Title } = Typography
 const InitialForm = ({ onLogin, children }) => {
   const [login, setLogin] = useState('')
   const [password, setPassword] = useState('')
@@ -12,88 +12,119 @@ const InitialForm = ({ onLogin, children }) => {
   }
   return (
     <>
+     
       <Form
-        className="Form"
         size="medium"
         name="basic"
-        style={{
-          margin: '0 auto',
-          margin: '50px',
-          padding: '20px',
-        }}
-        labelCol={{ span: 6 }}
-        wrapperCol={{ span: 13 }}
+        className="LoginForm-Form"
+        // labelCol={{ span: 12}}
+        // wrapperCol={{ span: 17 }}
+        // labelCol={{xs}}
         autoComplete="off"
       >
-        <h2 style={{}}> {children} </h2>
-
+        {/* <Col   xs={{ span: 10 }}
+        sm={16} md={12} lg={8} xl={4}> */}
+     
+ <Title level={2}> {children} </Title>
         <Form.Item
-          label="Login"
           name="login"
-          size="medium"
+          size="large"
           rules={[
             {
               required: true,
               message: 'Please input login!',
             },
           ]}
+          style={{marginBottom: '5px'}}
+          wrapperCol={{ offset: 4, span: 15 }}
         >
-          <Input
+           
+          <Input prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Login"
             value={login}
             size="medium"
             onChange={(e) => setLogin(e.target.value)}
           />
+          
+       
         </Form.Item>
-        <Text
-          type="secondary"
-          style={{ color: 'rgb(35, 60, 107)', margin: '20px 5px' }}
-        >
-          {' '}
-          Login must be at least 5 characters{' '}
-        </Text>
         <Form.Item
-          label="Password"
+         style={{margin:'0 auto'}}
+          wrapperCol={{ offset: 4, span: 14 }}
+        >    
+        <p
+          >
+            {' '}
+            * Login must be at least 5 characters{' '}
+          </p>
+          </Form.Item>
+        <Form.Item
           name="password"
-          size="medium"
+          size="large"
           rules={[
             {
               required: true,
               message: 'Please input your password!',
             },
           ]}
+          style={{ padding: '0 auto',marginBottom: '5px'}}
+
+          wrapperCol={{ offset: 4, span: 15 }}
         >
-          <Input
+           <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+            placeholder="Password"
             size="medium"
             type={checked ? 'text' : 'password'}
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+          
           />
+           
+          {/* <Input
+            size="medium"
+            type={checked ? 'text' : 'password'}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          /> */}
         </Form.Item>
-        <div style={{ width: '250px', marginLeft: '90px' }}>
-          <Text type="secondary" style={{ color: 'rgb(35, 60, 107)' }}>
+        <Form.Item
+         style={{margin:'0 auto'}}
+          wrapperCol={{ offset: 3, span: 15 }}
+        >   
+  <p>
+        * Use a combination of 8 or more letters, numbers, and symbols
+          </p>
+          </Form.Item>
+        {/* <Text type="secondary" style={{ color: 'rgb(35, 60, 107)' }}> */}
+        {/* style={{ width: '250px', marginLeft: '90px' }} */}
+
+        {/* <Text type="secondary" style={{ color: 'rgb(35, 60, 107)'}}>
             Password must contain at least eight characters, include letters,
             numbers and special symbols{' '}
-          </Text>
-        </div>
+          </Text> */}
+
         <Form.Item
           name="checked"
           valuePropName="checked"
-          wrapperCol={{ offset: 5, span: 10 }}
+          wrapperCol={{ offset: 3, span: 17 }}
+          style={{margin: '10px 0px'}}
+
         >
           <Checkbox
             checked={checked}
             onChange={(e) => {
               setChecked(e.target.checked)
             }}
-            size="large"
+            size="medium"
           >
             See the password
           </Checkbox>
         </Form.Item>
 
-        <Form.Item wrapperCol={{ offset: 6, span: 13 }}>
+        <Form.Item wrapperCol={{ offset: 4, span: 15 }}>
           <Button
-            size="medium"
+            size="large"
             type="primary"
             htmlType="submit"
             primary
@@ -108,8 +139,10 @@ const InitialForm = ({ onLogin, children }) => {
           >
             {children}
           </Button>
-        </Form.Item>
+          </Form.Item>
+        {/* </Col> */}
       </Form>
+    
     </>
   )
 }
--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -3,24 +3,47 @@ import { CLoginForm } from '../../components/authorization/LoginForm'
 import logo from '../../materials/login.png'
 import { Row, Col } from 'antd'
 import { Link } from 'react-router-dom'
-
+import { Typography } from 'antd'
+const {Title } = Typography
 export const LoginPageForm = () => (
   <div className="InputForm">
-    <Row>
-      <Col span={12}>
-        <img className="LoginPage" src={logo} />
+    <Row gutter={[16, 16]}>
+      
+      <Col
+        // span={12}
+        xl={{ span: 10 }}
+        lg={{ span: 8 }}
+        md={{ span: 0 }}
+        sm={{ span: 0}}   >
+        {/* xl={4} */}
+        
+        <img  src={logo} />
+       
       </Col>
 
-      <Col span={12}>
+
+      <Col xl={{ span: 14}}
+        lg={{ span: 16 }}
+        // md={{  span: 15 }}
+        sm={{ span: 10 }}
+         >
         <div className="LoginForm">
+          <section>
+          <Title style={{
+           
+            color: 'rgb(16, 30, 54)',
+            // width: '100%'float: 'left', 
+          }} level={3} >Welcome to Memogram! </Title>
+      
+          </section>
           <CLoginForm />
 
-          <h2>
-            Don't have an account yet?
+          <p>
+           Don't have an account yet?
             <Link to="/register" className="Link">
               Register
             </Link>
-          </h2>
+          </p>
         </div>
       </Col>
     </Row>
@@ -31,18 +54,18 @@ export const RegisterPageForm = () => (
   <div className="InputForm">
     <Row>
       <Col span={12}>
-        <img className="LoginPage" src={logo} />
+        <img src={logo} />
       </Col>
 
       <Col span={12}>
         <div className="LoginForm">
           <CRegisterForm />
-          <h2>
+          <p>
             Have an account?
             <Link to="/login" className="Link">
               Login
             </Link>
-          </h2>
+          </p>
         </div>
       </Col>
     </Row>
